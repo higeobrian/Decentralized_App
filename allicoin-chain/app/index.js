85306ccc86759865ce3577df2509ed15c6aadd5a
higeobrian <blacktiger55@hotmail.com>
2018-11-18 00:01:24 -0700

    live on port 3001

--- allicoin-chain/app/index.js
+++ allicoin-chain/app/index.js
@@ -7,5 +7,8 @@ const app = express();
 const bc = new Blockchain();
 
 app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
 
 });
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`)); 
